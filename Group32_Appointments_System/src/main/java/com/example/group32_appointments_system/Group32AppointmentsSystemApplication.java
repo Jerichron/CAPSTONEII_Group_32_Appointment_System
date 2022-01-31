56bd5547d64f0c51eaaf235eb26de97c4caaab87
Jerichron <adam.s.vandyke@gmail.com>
2022-01-31 11:01:30 -0500

    Initial Project Creation

---
+++ Group32_Appointments_System/src/main/java/com/example/group32_appointments_system/Group32AppointmentsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.group32_appointments_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group32AppointmentsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Group32AppointmentsSystemApplication.class, args);
+    }
+
+}
